64d4a5b057bb18c72770396024f3daeaf282826c
Jonathan Vollrath <jonathan@argospace.com>
2025-05-22 11:25:14 -0700

    Remove venv and update main for manual spring constants

--- radiator_deploy_sim/src/main.cpp
+++ radiator_deploy_sim/src/main.cpp
@@ -11,34 +11,15 @@
 
 int main() {
 
-    std::vector<double> k0{0.00051, 0.0019, 0.0014, 0.0013, 0.0004};
-    double minf;
+    Eigen::Matrix<double, 5, 1> k;
+    k << 
+        0.001561489,
+        0.003122977,
+        0.003122977,
+        0.003122977,
+        0.001561489;
 
-    const int n = 5;
-    nlopt::opt opt(nlopt::LN_NELDERMEAD, n);
-
-    std::vector<double> lb(n, 0.0001);
-    std::vector<double> ub(n, 5);
-    opt.set_lower_bounds(lb);
-    opt.set_upper_bounds(ub);
-
-    opt.set_min_objective(panels::objective, nullptr);
-    opt.set_xtol_rel(1e-4);
-    opt.set_maxeval(2000);
-
-    try {
-        nlopt::result res = opt.optimize(k0, minf);
-        std::cout << "Optimized stiffness values:\n";
-        for (double ki : k0) std::cout << ki << " ";
-        std::cout << "\nFinal cost: " << minf << "\n";
-        std::cout << "Number of Optimization Iterations: " << opt.get_numevals() << std::endl;
-
-        // Eigen::Matrix<double, 5, 1> theta = panels::simulate(k0);
-    } catch (std::exception& e) {
-        std::cerr << "NLopt failed: " << e.what() << "\n";
-    }
-
-
+    Eigen::Matrix<double, 5, 1> theta = panels::simulate(k);
 
     return 0;
 }
